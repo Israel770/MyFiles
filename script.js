8f69216ed0707919f04799eff95fc5a936529da2
Israel770 <Liatco89@gmail.com>
2019-03-26 22:59:38 +0200

    new
    new

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 alert("i am here");
+
+document.getElementById("okBtn").click(function (params) {
+   alert("ok button clicked") ;
+   alert(document.getElementById("okBtn").innerText());
+});
+
+document.getElementById("cancelBtn").click(function (params) {
+    alert("cancel button clicked") ;
+    alert(document.getElementById("cancelBtn").innerText());
+ });
