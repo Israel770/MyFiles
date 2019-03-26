12477629cd003740766713de1774a0b263faa08f
Israel770 <Liatco89@gmail.com>
2019-03-26 23:07:26 +0200

    aa

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 alert("i am here");
 
-document.getElementById("okBtn").click(function (params) {
+document.getElementById("okBtn").click(function () {
    alert("ok button clicked") ;
    alert(document.getElementById("okBtn").innerText());
-});
+}); 
 
-document.getElementById("cancelBtn").click(function (params) {
+document.getElementById("cancelBtn").click(function () {
     alert("cancel button clicked") ;
     alert(document.getElementById("cancelBtn").innerText());
  });
